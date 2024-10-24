8da848974c837ce2aebdea9b56e9a79f00707c24
assasin-hunterdev <noahthomson422@gmail.com>
2024-10-24 19:33:32 +0300

    upload

---
+++ VALT-FE/src/layouts/Eventview.tsx
@@ -0,0 +1,40 @@
+import Header from "../layouts/Header";
+import BackHeader from "../layouts/BackHeader";
+import Cardpre from "../Elements/card_pre";
+import Footer from "../layouts/Footer";
+import MainCpn from "../Elements/MainCpn";
+import TextSection from "../Elements/TextSection";
+import { useState , useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+
+const Eventview = () => {
+    const { id } = useParams();
+    const [data, setData] = useState([]);
+    async function  getCard(){
+        // const response = await axios.get(`https://valt-be.onrender.com/getevent/${id}`).then((res)=>{
+        const response = await axios.get(`https://valt-be.onrender.com/getevent/${id}`).then((res)=>{
+            setData(res.data);
+        })
+        console.log(response)
+    }
+    useEffect(()=>{
+        getCard();
+    },[])
+    return (
+        <div className="flex flex-col">
+            <div className="px-40 sm:px-4">
+                <Cardpre {...data}/>
+            </div>
+            <div className="px-40 sm:px-4 pt-8">
+                <TextSection {...data}/>
+            </div>
+            <div className="px-40 sm:px-4 pt-8">
+                <MainCpn  {...data}/>
+            </div>
+        </div>
+    )
+}
+
+export default Eventview;
---
+++ VALT-FE/src/wagmi.ts
@@ -0,0 +1,24 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  bsc,
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    bsc,
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.REACT_APP_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+});
